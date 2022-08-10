fe22186cbbe2a2b6957c7741416bcf4b1b558b4c
mustabbas2000@gmail.com <mustabbas2000@gmail.com>
2022-08-10 23:46:53 +0300

    implment shoping list and add shop view

--- src/app/shoping-list/shoping-list.component.ts
+++ src/app/shoping-list/shoping-list.component.ts
@@ -1,11 +1,16 @@
 import { Component, OnInit } from '@angular/core';
-
+import { ingredient } from '../shared/ingredient_model';
 @Component({
   selector: 'app-shoping-list',
   templateUrl: './shoping-list.component.html',
   styleUrls: ['./shoping-list.component.css']
 })
 export class ShopingListComponent implements OnInit {
+
+  ingredients: ingredient[] = [
+    new ingredient('apple',5),
+    new ingredient('tomato',10)
+  ];
 
   constructor() { }
 
